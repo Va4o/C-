1690039ce075d570ce42bcf2c0ffe7e55d6e5086
Va4o <104180897+Va4o@users.noreply.github.com>
2023-04-26 09:05:06 +0300

    Add project files.

---
+++ counting sheeps.cpp
@@ -0,0 +1,12 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int b = 100;
+    for (int a = 1; a <= b; a++) {
+        std::cout << a << " sheep...";
+    }
+    return 0;
+}
